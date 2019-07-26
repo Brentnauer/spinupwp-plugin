478f640c689213558304318673ef8d981027017a
Gilbert Pellegrom <gilbert@pellegrom.me>
2019-07-26 13:41:31 +0100

    Revert "Only purge public post types by default"

--- src/Cache.php
+++ src/Cache.php
@@ -116,10 +116,6 @@ class Cache {
 	public function purge_post_on_update( $new_status, $old_status, $post ) {
 		$post_type = get_post_type( $post );
 
-		if ( ! in_array( $post_type, $this->get_public_post_types() ) ) {
-			return false;
-		}
-
 		if ( in_array( $post_type, $this->get_post_types_excluded_from_purge() ) ) {
 			return false;
 		}
@@ -148,10 +144,6 @@ class Cache {
 	 */
 	public function purge_post_on_delete( $post_id ) {
 		$post_type = get_post_type( $post_id );
-
-		if ( ! in_array( $post_type, $this->get_public_post_types() ) ) {
-			return false;
-		}
 
 		if ( in_array( $post_type, $this->get_post_types_excluded_from_purge() ) ) {
 			return false;
@@ -191,14 +183,9 @@ class Cache {
 	 */
 	public function purge_post_by_comment( $comment_id ) {
 		$comment = get_comment( $comment_id );
+		$post    = get_post( $comment->comment_post_ID );
 
-		if ( $comment && $comment->comment_post_ID ) {
-			$post = get_post( $comment->comment_post_ID );
-
-			return $this->purge_post( $post );
-		}
-
-		return false;
+		return $this->purge_post( $post );
 	}
 
 	/**
@@ -381,19 +368,6 @@ class Cache {
 		}
 
 		return apply_filters( 'spinupwp_should_purge_post_status', true );
-	}
-
-	/**
-	 * Get public post types that should trigger a cache purge.
-	 *
-	 * @return array
-	 */
-	private function get_public_post_types() {
-		$post_types = get_post_types( [
-			'public' => true,
-		] );
-
-		return apply_filters( 'spinupwp_public_post_types', $post_types );
 	}
 
 	/**
